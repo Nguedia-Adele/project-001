98fe88f545d9279bc832f7e1cf4f5512d17f8e03
Nguedia Adele <nguediaadele@yahoo.fr>
2019-07-16 19:25:43 +0100

    Updates

---
+++ precache-manifest.c7930433f713612a36278841b7897d25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "693d17d42b2ec614833d89c947af1ab5",
+    "url": "/project-001/index.html"
+  },
+  {
+    "revision": "88ba1b3dae31a1ae824e",
+    "url": "/project-001/static/css/2.81a88bd3.chunk.css"
+  },
+  {
+    "revision": "2bcbe1c32b56171b4fd5",
+    "url": "/project-001/static/css/main.0c238cde.chunk.css"
+  },
+  {
+    "revision": "88ba1b3dae31a1ae824e",
+    "url": "/project-001/static/js/2.41b2c8cf.chunk.js"
+  },
+  {
+    "revision": "2bcbe1c32b56171b4fd5",
+    "url": "/project-001/static/js/main.b8673b1a.chunk.js"
+  },
+  {
+    "revision": "b193ac170a30ebe35db6",
+    "url": "/project-001/static/js/runtime~main.be9ef66d.js"
+  }
+]);
